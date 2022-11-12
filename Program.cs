f4a29926c4f1b89fc4136f9eb0d8853732d27c86
anassuv <114801727+anassuv@users.noreply.github.com>
2022-11-12 09:35:41 +0300

    all added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("{0} -> ", n);
+            for (int i = 1; i <= n; i++)            
+                Console.Write("{0}, ", i * i * i);
+            
+            Console.ReadKey();
+        }
+    }
+}
